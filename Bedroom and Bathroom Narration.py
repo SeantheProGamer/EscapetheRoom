f8d16d4f91cd7ebb81e57488f4a0ea01a486ce23
slessard19 <34479172+slessard19@users.noreply.github.com>
2017-12-21 08:29:05 -0500

    New Living Room and Bedroom Edits

--- Bedroom and Bathroom Narration.py
+++ Bedroom and Bathroom Narration.py
@@ -1,8 +1,8 @@
 #Narration for Bedroom and Bathroom
 
-print "WHEN ENTERING"
+print "WHEN ENTERING INITIALLY"
 
-print "You unlock the stairwell door and enter a dimly lit bedroom. You see the light of the bathroom gleaming in the corner."
+print "You unlock the stairwell door and enter a dimly lit bedroom. You see the light of the bathroom gleaming in the corner. You also see a white door on the opposite side of the bathroom."
 
 print "LISTING ITEMS IN BEDROOM"
 
@@ -16,9 +16,17 @@ print "LISTING ITEMS IN BATHROOM"
 
 print "In the bathroom, you see a shower, a sink, a medicine cabinet (medicine cabinet should contain a white key for the bedroom door), and a mirror."
 
-print "WHEN REENTERING BEDROOM"
+print "WHEN REENTERING BEDROOM FROM BATHROOM"
 
 print "You walk back into the bedroom from the bathroom."
+
+print "WHEN REENTERING BEDROOM FROM LIVING ROOM"
+
+print "You walk back into the bedroom from the living room."
+
+print "WHEN REENTERING BEDROOM FROM STAIRWELL"
+
+print "You walk back into the bedroom from the stairwell."
 
 
 
