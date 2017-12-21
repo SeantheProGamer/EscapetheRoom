a2370bb64200f16c5187b65b50d15859b2927f8c
swilliams18 <34479163+swilliams18@users.noreply.github.com>
2017-12-21 08:46:19 -0500

    "Oh yeah"
    -Kool aid man, 2008.

--- MovingThroughRooms.py
+++ MovingThroughRooms.py
@@ -1,27 +1,71 @@
 def room():
     room = 1
     while room < 4:    
+        key = 0
+        gold = 0
         room = 1
-        if room == 1:
-            print "You are now in room 1."
-            action1 = input("Choose. (Type forward with quotation marks)")
-            if action1 == "forward":
-                room = 2
+        while room == 1:
+            print "You are now in the basement. A cold, dark room underneath the house. It smells like rust. There is a door to the north, a rusty grandfather clock, a rusty drawer, and a rusty lamp."
+            action1 = input("(Type the direction you want to go or the item you want to interact with with quotation marks)")
+            if action1 == "north":
+                if key == 1:
+                    room = 2
+                else:
+                    print "It's locked."
             else:
-                room = 2
-        if room == 2:   
-            print "You are now in room 2."
-            action2 = input("Choose. (Type forward with quotation marks)")
-            if action2 == "forward":
-                room = 3
+                print "That is a wall."
+            if action1 == "Search the grandfather clock":
+                key = 1
+                print "You found a rusty key... sick."
+            if action1 == "Search the drawer":
+                print "Doesn't open, too rusty."
+            if action1 == "Search the lamp":
+                print "It's a lamp, it doesn't have storage space."
             else:
-                room = 3
-        if room == 3:   
-            print "You are now in room 3."
+                print "That aint an option."
+        while room == 2:   
+            print "You are now on the stairs."
+            action2 = input("Choose. (To the north is a door. 5 paintings grace the walls, each with a rusty name plate and a description. Seth: A handsome boy. Kevin: Cries himself to slept every night. Sean: Timid individual. Dylan: The best teacher. McDonough: A vile toadstool who isn't particularly good at teaching.)")
+            if action2 == "north":
+                if key == 2:
+                    room = 3
+                else:
+                    print "That door is also locked. Geez, two in a row."
+            if action2 == "Search Seth":
+                print "You gently look behind the painting... nothing."
+            if action2 == "Search Kevin":
+                print "Nope."
+            if action2 == "Search Sean":
+                print "Behind the painting is a hole in the wall. It's empty."
+            if action2 == "Search Dylan":
+                print "He's beautiful."
+            if action2 == "Search McDonough":
+                print "You bash his face in and find a key. Bonus!"
+                key = 2
+            else:
+                print "You sit down and think about death."
+        while room == 3:   
+            print "You are now in a bedroom with a rusty bed, a rusty dresser, a rusty nightstand, and a rusty wardrobe. There is also a door to the north. There is a bathroom to the east."
             action3 = input("Choose. (Type forward with quotation marks)")
-            if action3 == "forward":
-                room = 1
+            if action3 == "north":
+                if key == 3:
+                    room = 4
+                else:
+                    print "It's too locked."
+            if action3 == "Search bed":
+                print "You'd go to sleep, but it smells like rust."
+            if action3 == "Search dresser":
+                print "Don't get too excited."
+            if action3 == "Search nightstand":
+                print "Wow, that's alot of pills. Oh, there's also a strange golden key."
+                gold = 1
+            if action3 = "Search wardrobe":
+                print "That's alot of skeletons."
+            if action3 = "east"
+                room = 4
+                while room == 4:
+                    print "Your in a bathroom. Oh god, your drowning in rust."
             else:
-                room = 1
+                "Your getting tired of all this rust."
 
 room()
