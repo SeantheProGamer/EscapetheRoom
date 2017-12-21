10cd51197634dc190fa7870e3d7797ceb37e0433
slessard19 <34479172+slessard19@users.noreply.github.com>
2017-12-21 08:31:03 -0500

    Stairwell Edits

--- Stairwell Narration.py
+++ Stairwell Narration.py
@@ -1,8 +1,8 @@
 #Code for the Stairwell Narration
 
-print "WHEN ENTERING"
+print "WHEN ENTERING INITIALLY"
 
-print "I open the door of the stairwell, and I see several paintings on the walls."
+print "I open the door of the stairwell, and I see several paintings on the walls. There is a red door on the top of the stairs."
 
 print "LISTING PAINTINGS"
 
@@ -15,3 +15,11 @@ print "You search the painting of ... and find nothing."
 print "FINDING CORRECT PAINTING"
 
 print "You search the painting of Kevin and find a red key."
+
+print "REENTERING STAIRWELL FROM BASEMENT"
+
+print "You walk back into the stairwell from the basement."
+
+print "REENTERING STAIRWELL FROM BEDROOM"
+
+print "You walk back into the stairwell from the bedroom."
