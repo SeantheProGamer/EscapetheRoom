9e200370000d13dd025abb0a24482d376274ec3c
slessard19 <34479172+slessard19@users.noreply.github.com>
2017-12-21 08:04:10 -0500

    Narration for Stairwell

---
+++ Stairwell Narration.py
@@ -0,0 +1,17 @@
+#Code for the Stairwell Narration
+
+print "WHEN ENTERING"
+
+print "I open the door of the stairwell, and I see several paintings on the walls."
+
+print "LISTING PAINTINGS"
+
+print "The paintings include one of Kevin, one of McDonough, one of Dylan, one of Seth, and one of Sean."
+
+print "FINDING INCORRECT PAINTING"
+
+print "You search the painting of ... and find nothing."
+
+print "FINDING CORRECT PAINTING"
+
+print "You search the painting of Kevin and find a red key."
