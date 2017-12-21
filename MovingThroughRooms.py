fbbeb27b2c8e058616bf1d21b458f790577b7fd5
swilliams18 <34479163+swilliams18@users.noreply.github.com>
2017-12-21 09:00:00 -0500

    Live is meaningless torture.
    End me.

--- MovingThroughRooms.py
+++ MovingThroughRooms.py
@@ -24,8 +24,8 @@ def room():
             else:
                 print "That aint an option."
         while room == 2:   
-            print "You are now on the stairs."
-            action2 = input("Choose. (To the north is a door. 5 paintings grace the walls, each with a rusty name plate and a description. Seth: A handsome boy. Kevin: Cries himself to slept every night. Sean: Timid individual. Dylan: The best teacher. McDonough: A vile toadstool who isn't particularly good at teaching.)")
+            print "You are now on the stairs. To the north is a door. 5 paintings grace the walls, each with a rusty name plate and a description. Seth: A handsome boy. Kevin: Cries himself to slept every night. Sean: Timid individual. Dylan: The best teacher. McDonough: A vile toadstool who isn't particularly good at teaching."
+            action2 = input("Choose.")
             if action2 == "north":
                 if key == 2:
                     room = 3
@@ -46,10 +46,10 @@ def room():
                 print "You sit down and think about death."
         while room == 3:   
             print "You are now in a bedroom with a rusty bed, a rusty dresser, a rusty nightstand, and a rusty wardrobe. There is also a door to the north. There is a bathroom to the east."
-            action3 = input("Choose. (Type forward with quotation marks)")
+            action3 = input("Choose.")
             if action3 == "north":
                 if key == 3:
-                    room = 4
+                    room = 5
                 else:
                     print "It's too locked."
             if action3 == "Search bed":
@@ -59,13 +59,21 @@ def room():
             if action3 == "Search nightstand":
                 print "Wow, that's alot of pills. Oh, there's also a strange golden key."
                 gold = 1
-            if action3 = "Search wardrobe":
+            if action3 == "Search wardrobe":
                 print "That's alot of skeletons."
-            if action3 = "east"
+            if action3 == "east":
                 room = 4
                 while room == 4:
-                    print "Your in a bathroom. Oh god, your drowning in rust."
+                    print "Your in a bathroom. Oh god, your drowning in rust. Search it, search the bathroom."
+                    action4 = input("Choose.")
+                    if action3 == "Search the bathroom.":
+                        print "Oooooo, boy. Another rusty key. DELICIOUS!"
+                        room = 3
+                    else:
+                        print "Search the batheroom, Shawn."
             else:
                 "Your getting tired of all this rust."
+        while room == 5:
+            print "Aw dang it! A rusty living room!"
 
 room()
