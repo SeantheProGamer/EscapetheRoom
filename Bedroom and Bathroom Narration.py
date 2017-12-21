3ab3cd8987880345cdce9f9c6d37a07b19f86e84
slessard19 <34479172+slessard19@users.noreply.github.com>
2017-12-21 08:14:06 -0500

    Bathroom and Bathroom Narration

---
+++ Bedroom and Bathroom Narration.py
@@ -0,0 +1,18 @@
+#Narration for Bedroom and Bathroom
+
+print "WHEN ENTERING"
+
+print "You unlock the stairwell door and enter a dimly lit bedroom. You see the light of the bathroom gleaming in the corner."
+
+print "LISTING ITEMS IN BEDROOM"
+
+print "In the bedroom, you see a bed, a dresser, a nightstand (nightstand should contain a golden key for front door), and a wardrobe."
+
+print "ENTERING BATHROOM"
+
+print "You enter the bathroom from the bedroom."
+
+print "LISTING ITEMS IN BATHROOM"
+
+print "In the bathroom, you see a shower, a sink, a medicine cabinet, 
+
