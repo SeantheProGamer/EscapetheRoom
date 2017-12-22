b8e18ef9d6a0d56e616f5e9811449fc8d4619f4c
slessard19 <34479172+slessard19@users.noreply.github.com>
2017-12-22 00:42:45 -0500

    Revamped Code
    Includes changes with each section, as well as the addition of the bathroom.

--- MovingThroughRooms.py
+++ MovingThroughRooms.py
@@ -1,79 +1,175 @@
+import time
 def room():
-    room = 1
-    while room < 4:    
-        key = 0
-        gold = 0
-        room = 1
-        while room == 1:
-            print "You are now in the basement. A cold, dark room underneath the house. It smells like rust. There is a door to the north, a rusty grandfather clock, a rusty drawer, and a rusty lamp."
-            action1 = input("(Type the direction you want to go or the item you want to interact with with quotation marks)")
-            if action1 == "north":
-                if key == 1:
+    room = 0
+    blueKey = 0
+    redKey = 0
+    whiteKey = 0
+    silverKey = 0
+    goldKey = 0
+    timesBasementDoorOpened = 0
+    timesStairwellDoorOpened = 0
+    timesBedroomDoorOpened = 0
+    timesBathroomDoorOpened = 0
+    timesLivingRoomDoorOpened = 0
+    while room <= 6:
+        print 'You wake up in a dark and dreary basement. You are worried that you are trapped. You must find a way out! There is a blue door, a grandfather clock, a set of drawers, and a lamp.'
+        print ''
+        time.sleep(1)
+        while room == 0: #Basement
+            basementAction = input('Type, "Open the basement door" to try opening the door or type, "Search the ..." to search the item you want to interact with.')
+            print ''
+            if basementAction == 'Open the basement door' or 'open the basement door':
+                timesBasementDoorOpened += 1
+                if blueKey == 1:
+                    if timesBasementDoorOpened <= 1:
+                        print 'You are now in a stairwell. Up the stairs is a red door. 5 paintings grace the walls, each with a rusty name plate. The paintings are labeled "Seth", "Sean", "Kevin", "Dylan", and "McDonough".'
+                        print ''
+                        time.sleep(1)
+                    room = 1
+                else:
+                    print 'The door is locked.'
+                    print ''
+                    time.sleep(1)
+            elif basementAction == 'Search the grandfather clock' or 'search the grandfather clock':
+                blueKey = 1
+                print 'You found a blue key!'
+                print ''
+                time.sleep(1)
+            elif basementAction == 'Search the set of drawers' or 'search the set of drawers':
+                print "The drawers won't open. They seem to be sealed shut from rust."
+                print ''
+                time.sleep(1)
+            elif basementAction == 'Search the lamp' or 'search the lamp':
+                print 'There is nothing attached to or in the lamp.'
+                print ''
+                time.sleep(1)
+            else:
+                print "That isn't an available option."
+                print ''
+                time.sleep(1)
+        while room == 1: #Stairwell
+            stairwellAction = input('Type, "Open the stairwell door" to try opening top stairwell door, type, "Open the basement door" to reenter the basement, or type "Search the painting labeled ..." to search the painting you want to interact with.')
+            print ''
+            if stairwellAction == 'Open the stairwell door' or 'open the stairwell door':
+                timesStairwellDoorOpened += 1
+                if redKey == 1:
+                    if timesStairwellDoorOpened <= 1:
+                        print 'You unlock the stairwell door and enter a dimly lit bedroom. You see the light of the bathroom gleaming in the corner. You also see a white door on the opposite side of the bathroom. In the room is a bed, a dresser, a nightstand, and a wardrobe.'
+                        print ''
+                        time.sleep(1)
                     room = 2
                 else:
-                    print "It's locked."
+                    print 'The door is locked.'
+                    print ''
+                    time.sleep(1)
+            elif stairwellAction == 'Open the basement door' or 'open the basement door':
+                print 'You reenter the basement from the stairwell.'
+                print ''
+                time.sleep(1)
+                room = 1
+            elif stairwellAction == 'Search the painting labeled Seth' or 'search the painting labeled Seth':
+                print "You flip the painting around and find nothing."
+                print ''
+                time.sleep(1)
+            elif stairwellAction == 'Search the painting labeled Kevin' or 'search the painting labeled Kevin':
+                print 'No dice after searching this painting.'
+                print ''
+                time.sleep(1)
+            elif stairwellAction == 'Search the painting labeled Sean' or 'search the painting labeled Sean':
+                print "Behind the painting is a hole in the wall. Unfortunately, the hole is empty."
+                print ''
+                time.sleep(1)
+            elif stairwellAction == 'Search the painting labeled Dylan' or 'search the painting labeled Dylan':
+                print "He's beautiful, but there is nothing behind the painting."
+                print ''
+                time.sleep(1)
+            elif stairwellAction == 'Search the painting labeled McDonough' or 'search the painting labeled McDonough':
+                redKey = 1
+                print "You bash his face in and discover a hole in the wall. A red key is inside!"
+                print ''
+                time.sleep(1)
             else:
-                print "That is a wall."
-            if action1 == "Search the grandfather clock":
-                key = 1
-                print "You found a rusty key... sick."
-            if action1 == "Search the drawer":
-                print "Doesn't open, too rusty."
-            if action1 == "Search the lamp":
-                print "It's a lamp, it doesn't have storage space."
-            else:
-                print "That aint an option."
-        while room == 2:   
-            print "You are now on the stairs. To the north is a door. 5 paintings grace the walls, each with a rusty name plate and a description. Seth: A handsome boy. Kevin: Cries himself to slept every night. Sean: Timid individual. Dylan: The best teacher. McDonough: A vile toadstool who isn't particularly good at teaching."
-            action2 = input("Choose.")
-            if action2 == "north":
-                if key == 2:
-                    room = 3
+                print "That isn't an available option."
+                print ''
+                time.sleep(1)
+        while room == 2: #Bedroom
+            bedroomAction = input('Type, "Open the bedroom door" to try opening the bedroom door, type, "Open the stairwell door" to reenter the basement, type, "Open the bathroom door" to try entering the bathroom, or type "Search the ..." to search the item you want to interact with.')
+            print ''
+            if bedroomAction == 'Open the bedroom door' or 'open the bedroom door':
+                timesBedroomDoorOpened += 1
+                if whiteKey == 1:
+                    if timesBedroomDoorOpened <= 1:
+                        print 'You unlock the bedroom door and enter a brightly lit living room. Inside the room is a TV cabinet, a couch, a coffee table, and a lamp.'
+                        print ''
+                        time.sleep(1)
+                    room = 4
                 else:
-                    print "That door is also locked. Geez, two in a row."
-            if action2 == "Search Seth":
-                print "You gently look behind the painting... nothing."
-            if action2 == "Search Kevin":
-                print "Nope."
-            if action2 == "Search Sean":
-                print "Behind the painting is a hole in the wall. It's empty."
-            if action2 == "Search Dylan":
-                print "He's beautiful."
-            if action2 == "Search McDonough":
-                print "You bash his face in and find a key. Bonus!"
-                key = 2
+                    print "The door is locked."
+                    print ''
+                    time.sleep(1)
+            elif bedroomAction == 'Search the bed' or 'search the bed':
+                print "You search the sheets on the bed and find nothing. You'd sleep, but you have to get out of the house!"
+                print ''
+                time.sleep(1)
+            elif bedroomAction == 'Search the dresser' or 'search the dresser':
+                print "Don't get too excited, as nothing is inside.""
+                print ''
+                time.sleep(1)
+            elif bedroomAction == 'Search the nightstand' or 'search the nightstand':
+                whiteKey = 1
+                print "In the nightstand drawer, you find a white key!"
+                print ''
+                time.sleep(1)
+            elif bedroomAction == 'Search the wardrobe' or 'search the wardrobe':
+                print 'Sadly, nothing is inside.'
+                print ''
+                time.sleep(1)
+            elif bedroomAction == 'Open the bathroom door' or 'open the bathroom door':
+                timesBathroomDoorOpened += 1
+                if timesBathroomDoorOpened <= 1:
+                    print 'You enter a mostly white bathroom. Inside is a shower, a sink, a medicine cabinet, and a toilet.'
+                room = 3
             else:
-                print "You sit down and think about death."
-        while room == 3:   
-            print "You are now in a bedroom with a rusty bed, a rusty dresser, a rusty nightstand, and a rusty wardrobe. There is also a door to the north. There is a bathroom to the east."
-            action3 = input("Choose.")
-            if action3 == "north":
-                if key == 3:
-                    room = 5
-                else:
-                    print "It's too locked."
-            if action3 == "Search bed":
-                print "You'd go to sleep, but it smells like rust."
-            if action3 == "Search dresser":
-                print "Don't get too excited."
-            if action3 == "Search nightstand":
-                print "Wow, that's alot of pills. Oh, there's also a strange golden key."
-                gold = 1
-            if action3 == "Search wardrobe":
-                print "That's alot of skeletons."
-            if action3 == "east":
-                room = 4
-                while room == 4:
-                    print "Your in a bathroom. Oh god, your drowning in rust. Search it, search the bathroom."
-                    action4 = input("Choose.")
-                    if action3 == "Search the bathroom.":
-                        print "Oooooo, boy. Another rusty key. DELICIOUS!"
-                        room = 3
-                    else:
-                        print "Search the batheroom, Shawn."
+                print "That isn't an available option."
+                print ''
+                time.sleep(1)
+        while room == 3: #Bathroom
+            bathroomAction = input('Type, "Open the bathroom door" to exit the bathroom or type "Search the ..." to search the item you want to interact with.')
+            print ''
+            if bathroomAction == 'Open the bathroom door':
+                print 'You reenter the bedroom from the bathroom.'
+                print ''
+                time.sleep(1)
+                room = 2
+            elif bathroomAction == 'Search the shower' or 'search the shower':
+                print 'Nothing is on the floor of the shower.'
+                print ''
+                time.sleep(1)
+            elif bathroomAction == 'Search the sink' or 'search the sink':
+                print 'Nothing is in the sink.'
+                print ''
+                time.sleep(1)
+            elif bathroomAction == 'Search the medicine cabinet' or 'search the medicine cabinet':
+                goldKey = 1
+                print 'You find a golden key behind some pills in the medicine cabinet!'
+                print ''
+                time.sleep(1)
+            elif bathroomAction == 'Search the toilet' or 'search the toilet':
+                print "Don't even think about it. That thing looks disgusting."
+                print ''
+                time.sleep(1)
+            elif bathroomAction == 'Open the bathroom door' or 'open the bathroom door':
+                print 'You reenter the bedroom from the bathroom.'
+                print ''
+                time.sleep(1)
+                room = 2
             else:
-                "Your getting tired of all this rust."
-        while room == 5:
+                print "That isn't an available option."
+                print ''
+                time.sleep(1)
+        while room == 4: #Living Room
             print "Aw dang it! A rusty living room!"
+        while room == 5: #Foyer
+
 
 room()
